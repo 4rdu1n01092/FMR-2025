5321b016f5d9ff6d0dc510066be74c9c412771bb
Fear-sys <arthurpohl@icloud.com>
2025-09-29 18:36:52 -0300

    alpha 1.1: Atualizacao do main.py adicionando uma basica animacao de sequencia

--- FMR/__init__.py
+++ FMR/__init__.py
@@ -6,13 +6,20 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-
 # Definir blocos EV3 e motores
 ev3 = EV3Brick()
 RodaDireita = Motor(Port.B)
 RodaEsquerda = Motor(Port.C)
 robot = DriveBase(RodaEsquerda, RodaDireita, wheel_diameter=56, axle_track=114)
-#Inicio do programa
+# Definir blocos EV3 e motores
+ev3 = EV3Brick()
+RodaDireita = Motor(Port.B)
+RodaEsquerda = Motor(Port.C)
+robot = DriveBase(RodaEsquerda, RodaDireita, wheel_diameter=56, axle_track=114)
+#missoes 
+def limpezaRio():
+    print("executando percurso e coletando lixo")
+#sequencia de inicializacao
 def Iniciar():
     for i in range(3):
         ev3.screen.print("Inicializando")
@@ -28,7 +35,6 @@ def Iniciar():
         ev3.screen.clear()
         wait(1000)
     ev3.screen.print("Pronto!")
-    Iniciando = False
     wait(1000)
     ev3.screen.clear() 
 
@@ -37,3 +43,8 @@ Iniciar()
 print("Programa sendo iniciado")
 wait(13000)
 print("Programa Iniciado com sucesso!")
+print("Limpando rio...")
+limpezaRio()
+wait(50000)
+print("Limpeza concluida ✔️")
+wait(5)
