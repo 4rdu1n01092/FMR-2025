c0af9ef75439583b705bfde3882b76eca69a0f20
4rdu1n01092 <culbemaristafreirogerio@hotmail.com>
2025-10-01 09:57:56 -0300

    v1
    ele tenta andar

--- FMR/pegarFrutas.py
+++ FMR/pegarFrutas.py
@@ -7,26 +7,13 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-#Definir blocos EV3 e motores
+# Definir blocos EV3 e motores
 ev3 = EV3Brick()
 RodaDireita = Motor(Port.B)
 RodaEsquerda = Motor(Port.C)
 robot = DriveBase(RodaEsquerda, RodaDireita, wheel_diameter=56, axle_track=114)
-#missoes 
-
-print("executando percurso e coletando lixo")
-robot.straight(88)
-robot.turn(75)
-robot.straight(330)
-wait(20)
-robot.turn(65)
+#robot.straight(96)
+#robot.drive(100, -30)
+#wait(2700)
+#robot.stop()
 robot.straight(100)
-robot.turn(27)
-robot.straight(90)
-robot.turn(-25)
-robot.straight(90) 
-robot.turn(80)
-robot.straight(410)
-robot.turn(-35)
-robot.straight(40)
-
