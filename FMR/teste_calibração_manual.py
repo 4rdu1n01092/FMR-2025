3c22525027a7a47e30d2226bedf1f91fc14845ce
4rdu1n01092 <culbemaristafreirogerio@hotmail.com>
2025-10-22 15:04:37 -0300

    alpha 5.1
    ele calibra e executa mas naõ com perfeição.
    
    arq: teste_calibração_manual.py
    Co-Authored-By: RENGAW132 <171960500+RENGAW132@users.noreply.github.com>

--- FMR/teste_calibração_manual.py
+++ FMR/teste_calibração_manual.py
@@ -21,8 +21,43 @@ giroscopio = GyroSensor(Port.S2)
 #variaveis
 Nbifurcaçoes = 0
 ja_conte_bifurcacao = False
-VALOR_PRETO = 5
-VALOR_BRANCO = 95
+#VALOR_PRETO = 5
+#VALOR_BRANCO = 95
+VALOR_PRETO_ATUAL_D = 0
+VALOR_PRETO_ATUAL_E = 0
+VALOR_BRANCO_ATUAL_D = 0
+VALOR_BRANCO_ATUAL_E = 0
+LIMIAR_D = 0
+LIMIAR_E = 0
+def calibragem():
+    global VALOR_BRANCO_ATUAL_D
+    global VALOR_BRANCO_ATUAL_E
+    global VALOR_PRETO_ATUAL_D
+    global VALOR_PRETO_ATUAL_E
+    global LIMIAR_D
+    global LIMIAR_E
+    ev3.screen.draw_text(0, 0, "CALIBRAR PRETO") 
+    ev3.screen.draw_text(0, 20, "Centro p/ Continuar")
+    while not Button.CENTER in ev3.buttons.pressed():
+        wait(10)
+    VALOR_PRETO_ATUAL_D = SenseCorD.reflection()
+    VALOR_PRETO_ATUAL_E = SenseCorE.reflection()
+    #print(f"valor PRETO Direito={VALOR_PRETO_ATUAL_D}")
+    #print(f"valor PRETO Esquerdo={VALOR_PRETO_ATUAL_E}")
+    wait(1000)
+    ev3.screen.clear() 
+    ev3.screen.draw_text(0, 0, "CALIBRAR BRANCO")
+    ev3.screen.draw_text(0, 20, "Aperte Centro")
+    while not Button.CENTER in ev3.buttons.pressed():
+        wait(10)
+    VALOR_BRANCO_ATUAL_D = SenseCorD.reflection()
+    VALOR_BRANCO_ATUAL_E = SenseCorE.reflection()
+    #print(f"valor BRANCO Direito={VALOR_BRANCO_ATUAL_D}")
+    #print(f"valor BRANCO Esquerdo={VALOR_BRANCO_ATUAL_E}")
+    LIMIAR_D = (VALOR_PRETO_ATUAL_D + VALOR_BRANCO_ATUAL_D) / 2
+    LIMIAR_E = (VALOR_PRETO_ATUAL_E + VALOR_BRANCO_ATUAL_E) / 2
+
+
 #Definir Valores para calculo de erro
 kp = 3
 velo_base = 180
@@ -32,10 +67,14 @@ def seguefaixapreta(): #nome autoexplicatico (segue linha)
     global ja_conte_bifurcacao
     global kp
     global velo_base
+    global LIMIAR_D
+    global LIMIAR_E
     #calculos
     reflexãoEsq = SenseCorE.reflection()
     reflexãoDir = SenseCorD.reflection()
-    erro = reflexãoEsq - reflexãoDir
+    NORMALIZADO_E = reflexãoEsq-LIMIAR_E
+    NORMALIZADO_D = reflexãoDir-LIMIAR_D
+    erro = NORMALIZADO_E - NORMALIZADO_D
     correçao = kp * erro
     RodaDireita.run(velo_base-correçao)
     RodaEsquerda.run(velo_base+correçao)
@@ -49,6 +88,8 @@ def seguefaixapreta(): #nome autoexplicatico (segue linha)
     elif reflexãoEsq >= 60 and reflexãoDir >= 60 and ja_conte_bifurcacao:
         ja_conte_bifurcacao = False
 #Sequencia do programa
+calibragem()
+wait(10000)
 giroscopio.reset_angle(0)
 robot.straight(280)
 Stop.HOLD
---
+++ gabizin.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env pybricks-micropython
+# Importar bibliotecas necessárias do Pybricks
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# Definir blocos EV3 e motores
+ev3 = EV3Brick()
+RodaDireita = Motor(Port.B)
+RodaEsquerda = Motor(Port.C)
+motor_braco = Motor(Port.D)
+motor_garra = Motor(Port.A)
+sensor_cor = ColorSensor(Port.S3)
+robot = DriveBase(RodaEsquerda, RodaDireita, wheel_diameter=56, axle_track=114)
+
+#Funções auxiliares 
+def mover_braco(angulo, velocidade=200):
+    "Move o braço (e o sensor de cor) para cima ou para baixo."
+    motor_braco.run_angle(velocidade,angulo, Stop.BRAKE, True)
+
+def ler_cor():
+    "Lê a cor atual do sensor."
+    cor = sensor_cor.color()
+    if cor == Color.RED:
+        return "Vermelho"
+    elif cor == Color.BLUE:
+        return "Azul"
+    elif cor == Color.GREEN:
+        return "Verde"
+    else:
+        return "desconhecido"
+    
+def abrir_guarra():
+    motor_garra.run_angle(200, 120, Stop.BRAKE, True)
+
+def fechar_garra():
+    motor_garra.run_angle(200, -120, Stop.BRAKE, True)
+
+ev3.speaker.say("Iniciando leitura das cores")
+cores_memorizadas = []
+
+#O braço começa em cima e desce de forma gradual lendo as 3 cores
+for i in range (3):
+    mover_braco(-200, 200) #desce um pouco
+    wait(500)
+    cor_detectada = ler_cor()
+
+    cores_memorizadas.append(cor_detectada)
+    ev3.speaker.say(cor_detectada)
+    print(f"Cor detectada {i+1}:{cor_detectada}")
+    wait(500)
+
+#sobe o braço de volta à posição inicial
+mover_braco(600, 200)
+ev3.speaker.beep()
+print("Sequencia decorada(de baixo para cima):", cores_memorizadas)
+
+#agora o robô vai pegar as bolinhas nessa ordem
+ev3.speaker.say("Pegando as bolinhas")
+
+for cor in cores_memorizadas:
+    ev3.speaker.say(f"Pegar {cor}")
+    print(f"Pegar bolinha {cor}")
+
+    # Aqui você pode colocar lágica diferente para cada cor, se quiser
+    # Mas como as bolinhas estão juntas na caixa, ele só precisa fazer a sequência
+
+    abrir_guarra()
+    wait(300)
+    fechar_garra()
+    wait(1000)
+
+    #Após pegar uma bolinha, ele pode "soltar" em outro local ou só simular a ação
+    abrir_guarra()
+    wait(500)
+
+ev3.speaker.say ("Sequência completa")
+ev3.speaker.beep()
+print("Todas as bolinhas foram pegas na ordem certa")
